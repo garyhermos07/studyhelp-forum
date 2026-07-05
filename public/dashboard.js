3b5624ac2a243bed0faaac319c8d5a6c670f1b7d
Gary Hermosa <garyhermos2004@gmai.com>
2026-07-04 23:02:04 -0400

    Add question delete feature

---
+++ public/dashboard.js
@@ -0,0 +1,47 @@
+async function postQuestion() {
+
+    const title = document.querySelector("#questionTitle").value;
+    const content = document.querySelector("#questionContent").value;
+
+    const response = await fetch("/questions", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ title, content })
+    });
+
+
+    const data = await response.json();
+    alert(data.message);
+    loadQuestion();
+}
+
+async function loadQuestion() {
+    const response = await fetch("http://localhost:3000/questions");
+    const questions = await response.json();
+
+    const questionList = document.querySelector("#questionList");
+    questionList.innerHTML = "";
+
+    questions.forEach(question => {
+        questionList.innerHTML += `
+             <div>
+                <h3>${question.title}</h3>
+                <p>${question.content}</p>
+                <button onclick="deleteQuestion('${question._id}')">Delete</button>
+             </div>
+        `;
+    });
+}
+
+loadQuestion();
+
+async function deleteQuestion(id) {
+    await fetch(`http://localhost:3000/questions/${id}`, {
+        method: "DELETE"
+
+    });
+
+    loadQuestion();
+}
--- routes/questionRoutes.js
+++ routes/questionRoutes.js
@@ -1,8 +1,41 @@
 const express = require("express");
 const router = express.Router();
+const Question = require("../models/Question");
 
-router.get("/", (req, res) => {
-    res.send("Question Routes Working!");
+router.post("/", async (req, res) => {
+    try {
+        const { title, content } = req.body;
+
+        const newQuestion = new Question({
+            title,
+            content,
+            category: "General",
+            author: "User"
+        });
+        
+        await newQuestion.save();
+
+        res.json({ message: "Question posted successfuly!" });
+    } catch (err) {
+        res.status(500).json({
+            message: "Question failed",
+            error: err.message
+        });
+    }
+});
+
+router.get("/", async (req, res) => {
+    const questions = await Question.find();
+    res.json(questions);
+});
+
+router.delete("/:id", async (req, res) => {
+    try {
+        await Question.findByIdAndDelete(req.params.id);
+        res.json({ message: "Question deleted successfully!" });
+    } catch (err) {
+        res.status(500).json({ message: "Delete failed" });
+    }
 });
 
 module.exports = router;
